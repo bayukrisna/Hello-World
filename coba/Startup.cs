a1d8a0611e1e7ed67c1e7b8bafe2be009d0db328
Bayu Krisna <bayu_krisna_24rpl@student.smktelkom-mlg.sch.id>
2017-01-06 05:21:25 -0800

    Add project files.

---
+++ coba/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(coba.Startup))]
+namespace coba
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
